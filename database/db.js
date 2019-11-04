d59a6c86cdf8cbedef42338ff4e4aeaf1aefe89e
Adolphe <A-jhay@hotmail.com>
2019-11-05 00:00:07 +0100

    added remote database connection

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'sonnete',
-  password: 'root',
+  host: 'remotemysql.com',
+  user: 'JON8XloLiA',
+  database: 'JON8XloLiA',
+  password: '5Knug1MWWL',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
